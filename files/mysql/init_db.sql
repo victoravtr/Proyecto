17ea80b0e2ca263c6c50b55b47e6c0c0cedbb1c0
victoravtr <61812413+victoravtr@users.noreply.github.com>
2020-11-08 19:53:11 +0100

    Create init_db.sql

---
+++ files/mysql/init_db.sql
@@ -0,0 +1,14 @@
+create table CREDENCIALES (
+    usuario varchar(10) not null, 
+    pass varchar(255) not null, 
+    correo varchar(50) not null, 
+    constraint credenciales_pk primary key (usuario)
+);
+
+create table RESET_PASS (
+    correo varchar(50) not null,
+    codigo varchar(255) not null,
+    fecha_peticion date,
+    fecha_limite date,
+    constraint reset_pass_pk primary key(correo)
+);
