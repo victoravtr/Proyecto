2f3cd227b9f4a88630a6a25db05f1c73a40e778e
victoravtr <61812413+victoravtr@users.noreply.github.com>
2020-11-11 21:36:38 +0100

    Update exec_ssh.sh

--- Web-Proyecto-Content/assets/scripts/exec_ssh.sh
+++ Web-Proyecto-Content/assets/scripts/exec_ssh.sh
@@ -1,13 +1,11 @@
-main() {
-    IP=$1
-    USER=$2
-    PASS=$3
-    COMMAND="hostname"
-    RES=$(sshpass -p  $PASS ssh $USER@$IP $COMMAND);
-    if [ $? -eq 0 ]; then
-        echo $RES
-    fi
-    echo "$RES $PASS uwu"
-}
+IP=$1
+USER=$2
+PASS=$3
+COMMAND=$4
 
-main 
+RES=$(sshpass -p$PASS ssh -o StrictHostKeyChecking=no $USER@$IP $COMMAND);
+if ! [ $? -eq 0 ]; then
+    echo "Fallo."
+    exit 1
+fi
+echo $RES
