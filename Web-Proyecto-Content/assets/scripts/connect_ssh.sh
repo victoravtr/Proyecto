791bdff186ecdf49e5094223153236626112e90f
victoravtr <61812413+victoravtr@users.noreply.github.com>
2020-11-12 21:39:11 +0100

    Update connect_ssh.sh

--- Web-Proyecto-Content/assets/scripts/connect_ssh.sh
+++ Web-Proyecto-Content/assets/scripts/connect_ssh.sh
@@ -2,8 +2,6 @@ IP=$1
 USER=$2
 PASS=$3
 
-echo $PASS
-echo $3
 # Comprobamos si ssh esta instalado
 if ! [ -x "$(command -v ssh)" ]; then
     echo "1"
@@ -13,8 +11,8 @@ fi
 # Comprobamos si el equipo es accesible por ssh
 sshpass -p$PASS ssh -o StrictHostKeyChecking=no $USER@$IP exit
 if [ $? -eq 0 ]; then
-    echo "0"
+    echo true
     exit 1
 fi
-echo "1"
+echo false
 exit 1
