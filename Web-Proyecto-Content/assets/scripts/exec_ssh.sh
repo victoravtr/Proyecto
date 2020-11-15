363112f575a691607ab48d3afe388c9eb8d12772
victoravtr <61812413+victoravtr@users.noreply.github.com>
2020-11-15 15:20:38 +0100

    Update exec_ssh.sh

--- Web-Proyecto-Content/assets/scripts/exec_ssh.sh
+++ Web-Proyecto-Content/assets/scripts/exec_ssh.sh
@@ -1,7 +1,7 @@
 IP=$1
 USER=$2
 PASS=$3
-COMMAND=$4
+COMMAND="Test-Connection -ComputerName (hostname) -Count 1  | Select IPV4Address"
 
 RES=$(sshpass -p$PASS ssh -o StrictHostKeyChecking=no $USER@$IP $COMMAND);
 if ! [ $? -eq 0 ]; then
