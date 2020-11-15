f82df90607a9aad9fd08114cfdb0183cac2aa3e9
victoravtr <61812413+victoravtr@users.noreply.github.com>
2020-11-15 15:20:33 +0100

    Create connect_winrm.sh

---
+++ Web-Proyecto-Content/assets/scripts/connect_winrm.sh
@@ -0,0 +1,32 @@
+IP=$1
+USER=$2
+PASS=$3
+
+# Comprobamos si python esta instalado
+if ! [ -x "$(command -v python3)" ]; then
+    echo "1"
+    exit 1
+fi
+
+#Comprobamos si pip esta instalado
+if ! [ -x "$(command -v pip3)" ]; then
+    echo "1"
+    exit 1
+fi
+
+# Comprobamos si la libreria pywinrm esta instalada
+res=$(pip3 show pywinrm)
+if ! [ $? -eq 0 ]; then
+    echo "1"
+    exit 1
+fi
+
+# Ejecutamos el .py de comprobacion
+res=$(python3 connect_winrm.py $IP $USER $PASS)
+echo $res
+if [ $res -eq 0 ]; then
+    echo true
+    exit 1
+fi
+echo false
+exit 1
