796c5b436801c976c983c8e1e6188818b17522a2
victoravtr <61812413+victoravtr@users.noreply.github.com>
2020-11-15 15:21:11 +0100

    Update registro.php

--- Web-Proyecto-Content/registro.php
+++ Web-Proyecto-Content/registro.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Registrar usuario</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="assets/styles/style.css">
 
 </head>
 <body>
