c5ba73ff5b2d3c7704bbcfa7b388419d26b69c58
victoravtr <61812413+victoravtr@users.noreply.github.com>
2020-11-15 15:21:07 +0100

    Update login.php

--- Web-Proyecto-Content/login.php
+++ Web-Proyecto-Content/login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Iniciar sesión</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="assets/styles/style.css">
 
 </head>
 <body>
