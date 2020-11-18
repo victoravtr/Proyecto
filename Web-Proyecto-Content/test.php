c807ca8334526481d3d52c75a6f5ac1d622083f0
victorav <victoravtr@gmail.com>
2020-11-18 21:03:02 +0100

    Update test.php

--- Web-Proyecto-Content/test.php
+++ Web-Proyecto-Content/test.php
@@ -1,118 +1,130 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
 <head>
   <meta charset="UTF-8">
 </head>
+
 <body>
 
-<form action="test.php" method="post">
-  <input type="text" name="usuario" id="usuario">
-  <label for="comando">Usuario</label>
-  <br>
-  <input type="text" name="password" id="password">
-  <label for="password">Password</label>
-  <br>
-  <input type="text" name="ip" id="ip">
-  <label for="ip">IP</label>
-  <br>
-  <input type="submit" name="enviar">
-</form>
+  <form action="test.php" method="post">
+    <input type="text" name="usuario" id="usuario">
+    <label for="comando">Usuario</label>
+    <br>
+    <input type="text" name="password" id="password">
+    <label for="password">Password</label>
+    <br>
+    <input type="text" name="ip" id="ip">
+    <label for="ip">IP</label>
+    <br>
+    <select name="sistema">
+      <option value="windows">Windows 10</option>
+      <option value="linux">Linux</option>
+    </select>
+    <label for="sistema">Sistema operativo</label>
+    <br>
+    <input type="submit" name="enviar">
+  </form>
 
-<?php
+  <?php
 
 
-if (isset($_POST['enviar'])) {
-  $cadena_errores = "Error al procesar los datos del formulario:";
-  $error = false;
+  if (isset($_POST['enviar'])) {
+    $cadena_errores = "Error al procesar los datos del formulario:";
+    $error = false;
 
-  $IP=trim($_POST['ip']);
-  $USER=trim($_POST['usuario']);
-  $PASS=trim($_POST['password']);
-  echo "$IP $USER $PASS";
+    $IP = trim($_POST['ip']);
+    $USER = trim($_POST['usuario']);
+    $PASS = trim($_POST['password']);
+    $SISTEMA = trim($_POST['sistema']);
+    echo "$IP $USER $PASS";
 
-  $FILE_PATH= array (
-    "ssh" => "connect_ssh.sh",
-    "winrm" => "connect_winrm.sh",
-    "PSRemoting" => "connect_psremote.sh"
-  );
-  
-  
-  $user = trim(shell_exec("whoami"));
-  $hostname = trim(shell_exec("hostname"));
-  $path = trim(shell_exec("pwd"));
-  $COMMAND = "ipconfig";
-  $STRING = "";
-  $CONF_FILE = "/etc/proyecto/general/";
-  $PREF_METHOD = "";
+    $FILE_PATH = array(
+      "ssh" => "connect_ssh.sh",
+      "winrm" => "connect_winrm.sh",
+      "PSRemoting" => "connect_psremote.sh"
+    );
 
-  foreach($FILE_PATH as $key=>$value) {
-    echo "<p>Probando conexion ".$key."</p>";
-    echo "<p>Ejecutando ".$value."</p>";
-    $output = shell_exec("./assets/scripts/$value $IP $USER '$PASS'");
-    if ($output) {
-      echo "<p>Se ha establecido una conexion.</p>";
-      $STRING .= "${key}:true\n";
-      if ($key == "ssh") {
-        $PREF_METHOD = "ssh";
+
+    $user = trim(shell_exec("whoami"));
+    $hostname = trim(shell_exec("hostname"));
+    $path = trim(shell_exec("pwd"));
+    $COMMAND = "ipconfig";
+    $STRING = "";
+    $CONF_FILE = "/etc/proyecto/general/";
+    $PREF_METHOD = "";
+
+    foreach ($FILE_PATH as $key => $value) {
+      echo "<p>Probando conexion " . $key . "</p>";
+      echo "<p>Ejecutando " . $value . "</p>";
+      $output = shell_exec("./assets/scripts/$value $IP $USER '$PASS'");
+      if ($output) {
+        echo "<p>Se ha establecido una conexion.</p>";
+        $STRING .= "${key}:true\n";
+        if ($key == "ssh") {
+          $PREF_METHOD = "ssh";
+        }
+        if ($key == "winrm" && $PREF_METHOD != "ssh") {
+          $PREF_METHOD = "winrm";
+        }
+      } else {
+        echo "<p>No ha sido posible establecer una conexion.</p>";
+        $STRING .= "${key}:false\n";
       }
-      if ($key == "winrm" && $PREF_METHOD != "ssh") {
-        $PREF_METHOD = "winrm";
-      }
-    } else {
-      echo "<p>No ha sido posible establecer una conexion.</p>";
-      $STRING .= "${key}:false\n";
     }
+
+    $STRING .= "sistema:${SISTEMA}\n";
+    if ($PREF_METHOD == "") {
+      $STRING .= "pref_method:false";
+    } else {
+      $STRING .= "pref_method:${PREF_METHOD}\n";
+    }
+    $CONF_FILE .= $IP;
+    file_put_contents($CONF_FILE, $STRING);
+
+
+
+    // $IP=trim($_POST['ip']);
+    // $USER=trim($_POST['usuario']);
+    // $PASS=trim($_POST['password']);
+    // $COMMAND="whoami";
+
+    // if (empty($USER)) {
+    // 	$error=true;
+    // 	$cadena_errores = $cadena_errores."\\n No has introducido un usuario";
+    // }
+
+    // if (empty($PASS)) {
+    // 	$error=true;
+    // 	$cadena_errores = $cadena_errores."\\n No has introducido una pass";
+    // }
+
+    // if (empty($IP)) {
+    // 	$error=true;
+    // 	$cadena_errores = $cadena_errores."\\n No has introducido una IP";
+    // }
+
+    // if ($error) {
+    //   echo '<script> alert("'.$cadena_errores_bd.'")</script>';
+    // } else {
+    //   echo "<pre>Comprobando conexion por ssh: </pre>";
+    //   $output = shell_exec("bash assets/scripts/connect_ssh.sh $IP $USER $PASS");
+    //   if ($output==0) {
+    //     echo "<pre>La conexion por ssh ha sido exitosa.</pre>";
+    //     $output = shell_exec("bash assets/scripts/exec_ssh.sh $IP $USER '$PASS' $COMMAND");
+    //     echo $output;
+    //   } else {
+    //     echo "<pre>No se ha podido conectar por ssh.</pre>";
+    //   }
+    // }
+
+
   }
-  if ($PREF_METHOD == "") {
-    $STRING .= "pref_method:false";
-  } else {
-    $STRING .= "pref_method:${PREF_METHOD}\n";
-  }
-  $CONF_FILE .= $IP;
-  file_put_contents($CONF_FILE, $STRING);
 
 
 
-  // $IP=trim($_POST['ip']);
-  // $USER=trim($_POST['usuario']);
-  // $PASS=trim($_POST['password']);
-  // $COMMAND="whoami";
-  
-  // if (empty($USER)) {
-	// 	$error=true;
-	// 	$cadena_errores = $cadena_errores."\\n No has introducido un usuario";
-  // }
-
-  // if (empty($PASS)) {
-	// 	$error=true;
-	// 	$cadena_errores = $cadena_errores."\\n No has introducido una pass";
-  // }
-
-  // if (empty($IP)) {
-	// 	$error=true;
-	// 	$cadena_errores = $cadena_errores."\\n No has introducido una IP";
-  // }
-  
-  // if ($error) {
-  //   echo '<script> alert("'.$cadena_errores_bd.'")</script>';
-  // } else {
-  //   echo "<pre>Comprobando conexion por ssh: </pre>";
-  //   $output = shell_exec("bash assets/scripts/connect_ssh.sh $IP $USER $PASS");
-  //   if ($output==0) {
-  //     echo "<pre>La conexion por ssh ha sido exitosa.</pre>";
-  //     $output = shell_exec("bash assets/scripts/exec_ssh.sh $IP $USER '$PASS' $COMMAND");
-  //     echo $output;
-  //   } else {
-  //     echo "<pre>No se ha podido conectar por ssh.</pre>";
-  //   }
-  // }
-  
-
-}
-
-
-
-?>
+  ?>
 
 </body>
+
 </html>
