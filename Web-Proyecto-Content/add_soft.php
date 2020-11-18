776af5b05f939a83a2f6680b06c97315f7d9b384
victorav <victoravtr@gmail.com>
2020-11-18 21:03:05 +0100

    Update add_soft.php

--- Web-Proyecto-Content/add_soft.php
+++ Web-Proyecto-Content/add_soft.php
@@ -30,10 +30,17 @@ if (empty($usuario)) {
                         <input type="text" name="ip" placeholder="IP">
                         <input type="text" name="usuario" placeholder="Usuario">
                         <input type="password" name="password" placeholder="Password">
+                        <input type="file" name="file">
+                        <input type="submit" class="boton" name="add_soft" value="Añadir"/>
                     </div>
                     <div class="right-col">
-                        <input type="file" name="file">
-                        <input type="submit" class="boton" name="add_soft" value="Añadir" />
+                    <?php 
+                        if (isset($_POST["output"]) != "") { 
+                            foreach ($output as $out) {
+                                echo "<p>$out</p>";
+                            }
+                        } 
+                    ?>
                     </div>
 
                 </form>
@@ -44,52 +51,62 @@ if (empty($usuario)) {
     <?php
     if (isset($_POST["add_soft"])) {
         # Lista de extensiones permitidas: .exe .msi [para windows]  .deb [para linux]
-        $ALLOWED_EXTENSIONS = [".exe", ".msi", ".deb"];
+        $ALLOWED_EXTENSIONS = ["exe", "msi", "deb"];
         $error = false;
+        $output = array();
         $cadena_errores = "Error al procesar los datos del formulario:";
         # Comprobar que ningun campo esta vacio
         $ip = trim($_POST["ip"]);
         $usuario = trim($_POST["usuario"]);
-        $contraseña = trim($_POST["pass"]);
-        $file = trim($_POST["file"]);
-
+        $contraseña = trim($_POST["password"]);
+        $file = $_FILES["file"];
+        
+        array_push($output, "Comprobando formulario...");
         if (empty($ip)) {
             $error = true;
             $cadena_errores = $cadena_errores."\\n No has introducido una IP";
         }
+        array_push($output, "IP comprobada...");
 
         if (empty($usuario)) {
             $error = true;
             $cadena_errores = $cadena_errores."\\n No has introducido un usuario";
         }
+        array_push($output, "Usuario comprobado...");
 
         if (empty($contraseña)) {
             $error = true;
             $cadena_errores = $cadena_errores."\\n No has introducido una contraseña";
         }
+        array_push($output, "Password comprobada...");
 
         if (empty($file)) {
             $error = true;
             $cadena_errores = $cadena_errores."\\n No has introducido un archivo";
         } else {
-            if (!in_array(pathinfo($file)['extension'], $ALLOWED_EXTENSIONS)) {
+            if (!in_array(pathinfo($file["name"])['extension'], $ALLOWED_EXTENSIONS)) {
                 $error = true;
                 $cadena_errores = $cadena_errores."\\n Formato de archivo no permitido.";
+                echo pathinfo($file["name"])['extension'];
             }
         }
+        array_push($output, "Archivo comprobado...");
 
         if (!$error) {
             # Subimos el archivo al servidor
-            $BASE_DIR = "/var/www/uploads/";
-            $FILE_UPLOAD = $BASE_DIR.pathinfo($file)['filename'];
-            if (move_uploaded_file($FILE_UPLOAD , $file)) {
-                echo "El archivo se ha subido al servidor.";
+            $BASE_DIR = "/var/www/html/uploads/";
+            $FILE_UPLOAD = $BASE_DIR.$file["name"];
+            if (move_uploaded_file($file["tmp_name"] , $FILE_UPLOAD)) {
+                echo '<script> alert("El archivo se ha subido al servidor.")</script>';
+                array_push($output, "Archivo cargado en servidor...");
               } else {
                 $cadena_errores = $cadena_errores."\\n Ha ocurrido un error al intentar subir el archivo.";
                 echo '<script> alert("'.$cadena_errores_bd.'")</script>';
               }
             # Ejecutamos el script que lo descarga en la IP objetivo y lo instala
-            
+            array_push($output, "Ejecutando script de instalacion...");
+            $file_name = $file['name'];
+            $res = shell_exec("./assets/scripts/add_soft.sh $IP $USER '$PASS' $file_name");
             # Borramos el archivo del servidor
             
         } else {
