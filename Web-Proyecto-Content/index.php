d0bce7d6accde8a01adcac941924cedc4e4f772c
victorav <victoravtr@gmail.com>
2020-11-22 19:34:43 +0100

    Update index.php

--- Web-Proyecto-Content/index.php
+++ Web-Proyecto-Content/index.php
@@ -1,58 +1,64 @@
-<?php 
-    session_start();
-    $usuario = $_SESSION["usuario"];
-    if (empty($usuario)) {
-        header("Location:login.php");
-    }
+<?php
+session_start();
+$usuario = $_SESSION["usuario"];
+if (empty($usuario)) {
+    header("Location:login.php");
+}
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Index</title>
     <link rel="stylesheet" href="assets/styles/style_index.css">
 </head>
+
 <body>
 
-<a class="config" href="config.php">Configuracion </a>
-<a class="cerrar" href="logout.php">Cerrar sesión</a>
+    <a class="config" href="config.php">Configuracion </a>
+    <a class="cerrar" href="logout.php">Cerrar sesión</a>
 
-<div class="elementos">
-    <h1>Bienvenido <?php echo $_SESSION["usuario"]; ?></h1>    
+    <div class="elementos">
+        <h1>Bienvenido <?php echo $_SESSION["usuario"]; ?></h1>
 
-    <div class="card">
-        <div class="contenido">
-            <form action="index.php" method="post">
-                <input type="submit" class="boton" name="dominio" value="Añadir equipos a dominio"/>
-            </form>
+        <div class="card">
+            <div class="contenido">
+                <form action="index.php" method="post">
+                    <input type="submit" class="boton" name="dominio" value="Añadir equipos a dominio" />
+                </form>
 
-            <form action="index.php" method="post">
-                <input type="submit" class="boton" name="add_soft" value="Instalacion software en equipos"/>
-            </form>
-            
-            <form action="index.php" method="post">
-                <input type="submit" class="boton" name="zabbix" value="Añadir agente zabbix en equipo"/>
-            </form>
-            
-            <form action="index.php" method="post">
-                <input type="submit" class="boton" name="ossec" value="Añadir agente ossec en equipo"/>
-            </form>
+                <form action="index.php" method="post">
+                    <input type="submit" class="boton" name="add_soft" value="Instalacion software en equipos" />
+                </form>
 
-            <form action="index.php" method="post">
-                <input type="submit" class="boton" name="shell" value="Abrir shell en equipo"/>
-            </form>
+                <form action="index.php" method="post">
+                    <input type="submit" class="boton" name="zabbix" value="Añadir agente zabbix en equipo" />
+                </form>
+
+                <form action="index.php" method="post">
+                    <input type="submit" class="boton" name="ossec" value="Añadir agente ossec en equipo" />
+                </form>
+
+                <form action="index.php" method="post">
+                    <input type="submit" class="boton" name="shell" value="Abrir shell en equipo" />
+                </form>
+
+                <form action="index.php" method="post">
+                    <input type="submit" class="boton" name="operar" value="Operar/Configurar equipo" />
+                </form>
+
+            </div>
 
         </div>
-        
     </div>
-</div>
 
-<?php
+    <?php
     if (isset($_POST["dominio"])) {
         header("Location:dominio.php");
     }
-    
+
     // TODO: Crear add_soft.php y que haga sus cositas
     if (isset($_POST["add_soft"])) {
         header("Location:add_soft.php");
@@ -68,10 +74,16 @@
         header("Location:ossec.php");
     }
 
-        // TODO: Crear shell.php y que haga sus cositas
+    // TODO: Crear shell.php y que haga sus cositas
     if (isset($_POST["shell"])) {
         header("Location:shell.php");
     }
-?>
+
+    // TODO: Crear operar.php y que haga sus cositas
+    if (isset($_POST["operar"])) {
+        header("Location:operar.php");
+    }
+    ?>
 </body>
+
 </html>
