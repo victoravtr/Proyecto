f26285d91f3192477fc5891f0ebe5a2ee1493297
victorav <victoravtr@gmail.com>
2020-11-22 19:34:37 +0100

    Create join_domain.ps1

---
+++ Web-Proyecto-Content/assets/scripts/join_domain.ps1
@@ -0,0 +1,15 @@
+param (
+    [string]$cli_ip = $arg[1],
+    [string]$dom_ip = $arg[2],
+    [string]$dom_user = $arg[3],
+    [string]$dom_pass = $arg[4],
+    [string]$dom_name = $arg[5]
+)
+# Primero tenemos que conseguir el InterfaceIndex de la interfaz de red de la IP que ha introducido el usuario
+$InterfaceIndex = Get-NetIPAddress -IPAddress $cli_ip | findstr InterfaceIndex
+$InterfaceIndex = $InterfaceIndex.Split(":").replace(' ','')
+$InterfaceIndex = $InterfaceIndex[1]
+#Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses ("$dom_ip")
+$password = $( ConvertTo-SecureString  -String $dom_pass -AsPlainText -Force )
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList ($dom_user, $password)
+Add-Computer -DomainName $dom_name -Credential $credential -Restart
