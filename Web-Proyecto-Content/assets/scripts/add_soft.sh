e5eba198f0e62a43df9607435b6eb47604924eae
victorav <victoravtr@gmail.com>
2020-12-01 18:35:38 +0100

    Update add_soft.sh

--- Web-Proyecto-Content/assets/scripts/add_soft.sh
+++ Web-Proyecto-Content/assets/scripts/add_soft.sh
@@ -5,6 +5,7 @@ USER=$2
 PASS=$3
 FILE=$4
 SERVER_IP=$(hostname -I | sed 's/ *$//g')
+LOG=""
 # Comprobar conexion con equipo
 #   Leemos archivo, comprobando antes si existe, para ver si hay un metodo preferido.
 PREF_METHOD_FILE="/etc/proyecto/general/${IP}"
@@ -26,6 +27,7 @@ SISTEMA=$(cat /etc/proyecto/general/${IP} | grep sistema | cut -d ":" -f 2)
 # SI el sistema es Linux solo se podra usar ssh
 # Ademas dependiendo del sistema cambia el directorio de descargar y los comandos
 #   para hacer la instalacion
+EXT=$(${FILE: -3})
 
 if [ "$SISTEMA" == "Ubuntu" ] || [ "$SISTEMA" == "Debian" ]; then
     # Home Dir
@@ -35,36 +37,61 @@ if [ "$SISTEMA" == "Ubuntu" ] || [ "$SISTEMA" == "Debian" ]; then
     if [ $USER != "root" ]; then
         DIR="/home/${USER}/"
     fi
-    
+
     # Descargamos el archivo en el equipo objetivo
     COMMAND="curl -o ${DIR}${FILE} http://${SERVER_IP}/uploads/${FILE}"
     RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
     if ! [ $? -eq 0 ]; then
-        echo "Fallo al descargar el archivo."
+        LOG="$LOG \n Fallo al descargar el archivo."
         exit 1
     fi
 
-    # Instalamos el archivo con dpkg
-    # Tenemos que setear DEBIAN_FRONTEND=noninteractive antes de instalarlo para que no salga prompts
-    # Despues de instalar el programa seteamos a "" la variable
-    COMMAND="export DEBIAN_FRONTEND=noninteractive && dpkg -i ${DIR}${FILE} && export DEBIAN_FRONTEND="
-    RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
-    if ! [ $? -eq 0 ]; then
-        # Instalamos las dependencias si falla y volvemos a ejecutar el dpkg -i
-        COMMAND="apt-get -f -y install"
-        RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
-        if ! [ $? -eq 0 ]; then
-            echo "Fallo al instalar dependencias."
-            exit 1
-        fi
+    if [ "$EXT" == "deb" ]; then
+        # Instalamos el archivo con dpkg
+        # Tenemos que setear DEBIAN_FRONTEND=noninteractive antes de instalarlo para que no salga prompts
+        # Despues de instalar el programa seteamos a "" la variable
         COMMAND="export DEBIAN_FRONTEND=noninteractive && dpkg -i ${DIR}${FILE} && export DEBIAN_FRONTEND="
         RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
         if ! [ $? -eq 0 ]; then
-            echo "Fallo al instalar."
-            exit 1
+            # Instalamos las dependencias si falla y volvemos a ejecutar el dpkg -i
+            COMMAND="apt-get -f -y install"
+            RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
+            if ! [ $? -eq 0 ]; then
+                echo "Fallo al instalar dependencias."
+                exit 1
+            fi
+            COMMAND="export DEBIAN_FRONTEND=noninteractive && dpkg -i ${DIR}${FILE} && export DEBIAN_FRONTEND="
+            RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
+            if ! [ $? -eq 0 ]; then
+                echo "Fallo al instalar."
+                exit 1
+            fi
         fi
+    elif [ "$EXT" == "rpm" ]; then
+        # Instalamos el archivo con rpm
+        # Tenemos que setear DEBIAN_FRONTEND=noninteractive antes de instalarlo para que no salga prompts
+        # Despues de instalar el programa seteamos a "" la variable
+        COMMAND="export DEBIAN_FRONTEND=noninteractive && rpm -i ${DIR}${FILE} && export DEBIAN_FRONTEND="
+        RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
+        if ! [ $? -eq 0 ]; then
+            # Instalamos las dependencias si falla y volvemos a ejecutar el dpkg -i
+            COMMAND="apt-get -f -y install"
+            RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
+            if ! [ $? -eq 0 ]; then
+                echo "Fallo al instalar dependencias."
+                exit 1
+            fi
+            COMMAND="export DEBIAN_FRONTEND=noninteractive && rpm -i ${DIR}${FILE} && export DEBIAN_FRONTEND="
+            RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
+            if ! [ $? -eq 0 ]; then
+                echo "Fallo al instalar."
+                exit 1
+            fi
+        fi
+    else
+        echo "Tipo de archivos no permitidos."
+        exit 1
     fi
-
     # Borramos el archivo en el cliente
     COMMAND="rm ${DIR}${FILE}"
     RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
@@ -82,14 +109,10 @@ if [ "$SISTEMA" == "Ubuntu" ] || [ "$SISTEMA" == "Debian" ]; then
 fi
 
 if [ $SISTEMA == "windows" ]; then
+
     # Si pref_method es ssh ejecutamos las ordenes aqui, si es winrm ejecutamos el script de python
     if [ $PREF_METHOD == "ssh" ]; then
-        # Comprobamos conexion
-        RES=$(sshpass -p$PASS ssh -o StrictHostKeyChecking=no $USER@$IP exit)
-        if ! [ $? -eq 0 ]; then
-            echo "Fallo al realizar la conexion. ${RES}"
-            exit 1
-        fi
+
         # Descargamos el archivo en el equipo objetivo
         COMMAND="curl -o C:\Users\\${USER}\\${FILE} http://${SERVER_IP}/uploads/${FILE}"
         RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
@@ -97,15 +120,24 @@ if [ $SISTEMA == "windows" ]; then
             echo "Fallo al descargar el archivo."
             exit 1
         fi
-        # Ejecutamos el archivo
-        # Start-Process -FilePath C:\Users\victorav\chrome.exe -ArgumentList "/silent","/install" -PassThru -Verb runas;
-        COMMAND="Start-Process -FilePath \"C:\Users\\${USER}\\${FILE}\" -ArgumentList \"/install\",\"/silent\" -PassThru -Verb runas"
-        RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
 
-        if ! [ $? -eq 0 ]; then
-            echo "Fallo al ejecutar el archivo."
-            exit 1
+        if [ "$EXT" == "exe" ]; then
+            # Ejecutamos el archivo
+            # Start-Process -FilePath C:\Users\victorav\chrome.exe -ArgumentList "/silent","/install" -PassThru -Verb runas;
+            COMMAND="Start-Process -FilePath \"C:\Users\\${USER}\\${FILE}\" -ArgumentList \"/install\",\"/silent\" -PassThru -Verb runas"
+            RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
+            if ! [ $? -eq 0 ]; then
+                echo "Fallo al ejecutar el archivo."
+                exit 1
+            fi
+        elif [ "$EXT" == "msi" ]; then
+            COMMAND="msiexec.exe /i \"C:\Users\\${USER}\\${FILE}\" /qn"
+            if ! [ $? -eq 0 ]; then
+                echo "Fallo al ejecutar el archivo."
+                exit 1
+            fi
         fi
+
         # Una vez instalado borramos el archivo
         COMMAND="rm \"C:\Users\\${USER}\\${FILE}\""
         RES=$(sshpass -p$PASS ssh -t -o StrictHostKeyChecking=no $USER@$IP $COMMAND)
