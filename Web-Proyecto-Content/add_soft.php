1900f7f8d796e5690ce6351fee768b004c3db601
victorav <victoravtr@gmail.com>
2020-12-01 18:35:35 +0100

    Update add_soft.php

--- Web-Proyecto-Content/add_soft.php
+++ Web-Proyecto-Content/add_soft.php
@@ -27,20 +27,22 @@ if (empty($usuario)) {
             <div class="contenido">
                 <form action="add_soft.php" enctype="multipart/form-data" method="post">
                     <div class="left-col">
+                        <h2>Datos:</h2>
                         <input type="text" name="ip" placeholder="IP">
                         <input type="text" name="usuario" placeholder="Usuario">
                         <input type="password" name="password" placeholder="Password">
                         <input type="file" name="file">
-                        <input type="submit" class="boton" name="add_soft" value="Añadir"/>
+                        <input type="submit" class="boton" name="add_soft" value="Añadir" />
                     </div>
                     <div class="right-col">
-                    <?php 
-                        if (isset($_POST["output"]) != "") { 
+                        <h2>Outpout:</h2>
+                        <?php
+                        if (isset($_POST["output"]) != "") {
                             foreach ($output as $out) {
                                 echo "<p>$out</p>";
                             }
-                        } 
-                    ?>
+                        }
+                        ?>
                     </div>
 
                 </form>
@@ -50,8 +52,9 @@ if (empty($usuario)) {
 
     <?php
     if (isset($_POST["add_soft"])) {
+        echo shell_exec("whoami");
         # Lista de extensiones permitidas: .exe .msi [para windows]  .deb [para linux]
-        $ALLOWED_EXTENSIONS = ["exe", "msi", "deb"];
+        $ALLOWED_EXTENSIONS = ["exe", "msi", "deb", "rpm"];
         $error = false;
         $output = array();
         $cadena_errores = "Error al procesar los datos del formulario:";
@@ -60,60 +63,59 @@ if (empty($usuario)) {
         $usuario = trim($_POST["usuario"]);
         $contraseña = trim($_POST["password"]);
         $file = $_FILES["file"];
-        
+
         array_push($output, "Comprobando formulario...");
         if (empty($ip)) {
             $error = true;
-            $cadena_errores = $cadena_errores."\\n No has introducido una IP";
+            $cadena_errores = $cadena_errores . "\\n No has introducido una IP";
         }
         array_push($output, "IP comprobada...");
 
         if (empty($usuario)) {
             $error = true;
-            $cadena_errores = $cadena_errores."\\n No has introducido un usuario";
+            $cadena_errores = $cadena_errores . "\\n No has introducido un usuario";
         }
         array_push($output, "Usuario comprobado...");
 
         if (empty($contraseña)) {
             $error = true;
-            $cadena_errores = $cadena_errores."\\n No has introducido una contraseña";
+            $cadena_errores = $cadena_errores . "\\n No has introducido una contraseña";
         }
         array_push($output, "Password comprobada...");
 
         if (empty($file)) {
             $error = true;
-            $cadena_errores = $cadena_errores."\\n No has introducido un archivo";
+            $cadena_errores = $cadena_errores . "\\n No has introducido un archivo";
         } else {
             if (!in_array(pathinfo($file["name"])['extension'], $ALLOWED_EXTENSIONS)) {
                 $error = true;
-                $cadena_errores = $cadena_errores."\\n Formato de archivo no permitido.";
+                $cadena_errores = $cadena_errores . "\\n Formato de archivo no permitido.";
                 echo pathinfo($file["name"])['extension'];
             } else {
                 array_push($output, "Archivo comprobado...");
             }
         }
-        
+
 
         if (!$error) {
             # Subimos el archivo al servidor
             $BASE_DIR = "/var/www/html/uploads/";
             $FILE_UPLOAD = $BASE_DIR.$file["name"];
-            if (move_uploaded_file($file["tmp_name"] , $FILE_UPLOAD)) {
+            if (move_uploaded_file($file["tmp_name"], $FILE_UPLOAD)) {
                 array_push($output, "Archivo cargado en servidor...");
-              } else {
-                $cadena_errores = $cadena_errores."\\n Ha ocurrido un error al intentar subir el archivo.";
-                echo '<script> alert("'.$cadena_errores_bd.'")</script>';
-              }
+            } else {
+                $cadena_errores = $cadena_errores . "\\n Ha ocurrido un error al intentar subir el archivo.";
+                echo '<script> alert("' . $cadena_errores . '")</script>';
+            }
             # Ejecutamos el script que lo descarga en la IP objetivo y lo instala
             array_push($output, "Ejecutando script de instalacion...");
             $file_name = $file['name'];
             $res = shell_exec("./assets/scripts/add_soft.sh $ip $usuario '$contraseña' $file_name");
             # Borramos el archivo del servidor
-            
+
         } else {
-            echo '<script> alert("'.$cadena_errores_bd.'")</script>';
+            echo '<script> alert("' . $cadena_errores_bd . '")</script>';
         }
-        
     }
     ?>
 </body>
