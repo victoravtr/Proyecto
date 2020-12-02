d6d74af0689c49f78734c46b6045c6d6c995e916
victorav <victoravtr@gmail.com>
2020-12-02 22:11:25 +0100

    Update connect_ssh.sh

--- Web-Proyecto-Content/assets/scripts/connect_ssh.sh
+++ Web-Proyecto-Content/assets/scripts/connect_ssh.sh
@@ -1,18 +1,14 @@
+#!/bin/bash
+
 IP=$1
 USER=$2
 PASS=$3
-
-# Comprobamos si ssh esta instalado
-if ! [ -x "$(command -v ssh)" ]; then
-    echo "1"
-    exit 1
-fi
 
 # Comprobamos si el equipo es accesible por ssh
 sshpass -p$PASS ssh -o StrictHostKeyChecking=no $USER@$IP exit
 if [ $? -eq 0 ]; then
     echo true
-    exit 1
+    exit 0
 fi
 echo false
 exit 1
