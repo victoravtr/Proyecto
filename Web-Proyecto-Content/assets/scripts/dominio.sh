16d20cafe11d5f47a69fa6e0aa2734d7925ed57a
victorav <victoravtr@gmail.com>
2020-12-02 12:59:48 +0100

    Update dominio.sh

--- Web-Proyecto-Content/assets/scripts/dominio.sh
+++ Web-Proyecto-Content/assets/scripts/dominio.sh
@@ -9,27 +9,32 @@ DOM_USER=$5
 DOM_PASSWORD=$6
 DOM_NAME=$7
 
+STR=""
 SERVER_IP=$(hostname -I | sed 's/ *$//g')
 
 # Comprobamos si ya se ha testeado la conexion
 PREF_METHOD_FILE="/etc/proyecto/general/${CLI_IP}"
 if ! [ -f "$PREF_METHOD_FILE" ]; then
-    echo "El archivo de conexion para el cliente no existe, se creara automaticamente usando http://proyecto.local/test.php "
+    STR="$STR\n 1!@El archivo de conexion para el cliente no existe, se creara automaticamente usando http://proyecto.local/test.php "
+    echo $STR
     exit 1
 fi
 PREF_METHOD_FILE="/etc/proyecto/general/${DOM_IP}"
 if ! [ -f "$PREF_METHOD_FILE" ]; then
-    echo "El archivo de conexion para el dominio no existe, se creara automaticamente usando http://proyecto.local/test.php "
+    STR="$STR\n 1!@El archivo de conexion para el dominio no existe, se creara automaticamente usando http://proyecto.local/test.php "
+    echo $STR
     exit 1
 fi
 PREF_METHOD_CLI=$(tail -n 1 /etc/proyecto/general/${CLI_IP} | cut -d ":" -f 2)
 if [ "$PREF_METHOD_CLI" == "false" ]; then
-    echo "Debes habilitar alguna forma de comunicacion en el cliente y ejecutar http://proyecto.local/test.php "
+    STR="$STR\n 1!@Debes habilitar alguna forma de comunicacion en el cliente y ejecutar http://proyecto.local/test.php "
+    echo $STR
     exit 1
 fi
 PREF_METHOD_DOM=$(tail -n 1 /etc/proyecto/general/${DOM_IP} | cut -d ":" -f 2)
 if [ "$PREF_METHOD_DOM" == "false" ]; then
-    echo "Debes habilitar alguna forma de comunicacion en el dominio y ejecutar http://proyecto.local/test.php "
+    STR="$STR\n 1!@Debes habilitar alguna forma de comunicacion en el dominio y ejecutar http://proyecto.local/test.php "
+    echo $STR
     exit 1
 fi
 
@@ -37,64 +42,70 @@ fi
 SISTEMA_CLI=$(grep </etc/proyecto/general/${CLI_IP} sistema | cut -d ":" -f 2)
 SISTEMA_DOM=$(grep </etc/proyecto/general/${DOM_IP} sistema | cut -d ":" -f 2)
 if [ "$SISTEMA_CLI" != "windows" ]; then
-    echo "Solo se permiten clientes Windows."
+    STR="$STR\n 1!@Solo se permiten clientes Windows."
+    echo $STR
     exit 1
 fi
 if [ "$SISTEMA_DOM" != "windows" ]; then
-    echo "Solo se permiten dominios Windows."
+    STR="$STR\n 1!@Solo se permiten dominios Windows."
+    echo $STR
     exit 1
 fi
-
 
 if [ $PREF_METHOD_CLI == "ssh" ]; then
     # Copiamos el script de instalacion y lo descargamos en el equipo objetivo
     # cp /var/www/proyecto/assets/scripts/join_domain.ps1 /var/www/html/uploads
     cp join_domain.ps1 /var/www/html/uploads/join_domain.ps1
     if ! [ $? -eq 0 ]; then
-        echo "Fallo al copiar el archivo en /uploads."
+        STR="$STR\n 1!@Fallo al copiar el archivo en /uploads."
+        echo $STR
         exit 1
     fi
 
     # Generamos un archivo con la pass y lo descargamos en el cliente
     # Una vez descargado lo borramos del servidor
-    echo $DOM_PASSWORD > /var/www/html/uploads/pass.txt
+    echo $DOM_PASSWORD >/var/www/html/uploads/pass.txt
     COMMAND="curl -o C:\Users\\${CLI_USER}\\pass.txt http://${SERVER_IP}/uploads/pass.txt"
     RES=$(sshpass -p$CLI_PASSWORD ssh -t -o StrictHostKeyChecking=no $CLI_USER@$CLI_IP $COMMAND)
     if ! [ $? -eq 0 ]; then
-        echo "Fallo al descargar el archivo."
+        STR="$STR\n 1!@Fallo al descargar el archivo en el cliente.."
+        echo $STR
         exit 1
     fi
-    echo "Se descarga archivo en cliente"
+    STR="$STR\n 0!@Se descarga el archivo pass en el cliente."
+
     rm /var/www/html/uploads/pass.txt
     if ! [ $? -eq 0 ]; then
-        echo "Fallo al borrar el archivo en /uploads."
+        STR="$STR\n 1!@Fallo al borrar el archivo en /uploads."
+        echo $STR
         exit 1
     fi
 
     COMMAND="curl -o C:\Users\\${CLI_USER}\\join_domain.ps1 http://${SERVER_IP}/uploads/join_domain.ps1"
     RES=$(sshpass -p$CLI_PASSWORD ssh -t -o StrictHostKeyChecking=no $CLI_USER@$CLI_IP $COMMAND)
     if ! [ $? -eq 0 ]; then
-        echo "Fallo al descargar el archivo."
+        STR="$STR\n 1!@Fallo al descargar el script de powershell en el cliente."
+        echo $STR
         exit 1
     fi
-    echo "Se descarga archivo en cliente"
+    STR="$STR\n 0!@Se descarga el script de powershell en el cliente."
+    rm /var/www/html/uploads/join_domain.ps1
 
     # Una vez descargado lo ejecutamos en el cliente
     COMMAND="powershell.exe C:\Users\\${CLI_USER}\\join_domain.ps1 $CLI_IP $DOM_IP $DOM_USER $DOM_PASSWORD $DOM_NAME"
     RES=$(sshpass -p$CLI_PASSWORD ssh -t -o StrictHostKeyChecking=no $CLI_USER@$CLI_IP $COMMAND)
     if ! [ $? -eq 0 ]; then
-        echo "Fallo al descargar el archivo."
+        STR="$STR\n 1!@Fallo al ejecutar el script en el cliente: $RES"
+        echo $STR
         exit 1
     fi
-    echo "Se mete en dominio"
+    STR="$STR\n 0!@El equipo se ha unido al dominio."
+    echo $STR
+    exit 0
 fi
 
 if [ $PREF_METHOD_CLI == "winrm" ]; then
-    echo "Solo se permite la inclusion en dominios por medio de SSH"
+    STR="$STR\n 1!@Esta operacion solo esta permitida mediante SSH."
+    echo $STR
     exit 1
-    # RES=$(python3 /home/victor/Documentos/GitHub/Proyecto/Web-Proyecto-Content/assets/scripts/domain.py $CLI_IP $CLI_USER $CLI_PASSWORD $DOM_IP $DOM_USER $DOM_PASSWORD $DOM_NAME)
-    # if ! [ $RES -eq 0 ]; then
-    #     echo "Fallo al ejecutar script."
-    #     exit
-    # fi
 fi
