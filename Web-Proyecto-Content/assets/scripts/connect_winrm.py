9c668ae3803ed73640e76baf381396f17d79a43c
victorav <victoravtr@gmail.com>
2020-12-03 19:33:10 +0100

    Update connect_winrm.py

--- Web-Proyecto-Content/assets/scripts/connect_winrm.py
+++ Web-Proyecto-Content/assets/scripts/connect_winrm.py
@@ -3,6 +3,7 @@ import sys
 
 try:
     s = winrm.Session(sys.argv[1], auth=(sys.argv[2], sys.argv[3]))
+    r = s.run_cmd('ipconfig', ['/all'])
     print("0")
 except Exception as e:
     print("1")
