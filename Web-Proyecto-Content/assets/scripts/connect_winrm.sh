798b315a1909e3591bd4426d1ebe1c0872c6e833
victorav <victoravtr@gmail.com>
2020-12-05 15:11:37 +0100

    Update connect_winrm.sh

--- Web-Proyecto-Content/assets/scripts/connect_winrm.sh
+++ Web-Proyecto-Content/assets/scripts/connect_winrm.sh
@@ -6,7 +6,7 @@ PASS=$3
 
 # Ejecutamos el .py de comprobacion
 
-res=$(python3 /home/victor/Documentos/GitHub/Proyecto/Web-Proyecto-Content/assets/scripts/connect_winrm.py $IP $USER $PASS)
+res=$(python3 /var/www/proyecto/assets/scripts/connect_winrm.py $IP $USER $PASS)
 if [ $res -eq 0 ]; then
     echo true
     exit 0
