3e6f65fe83c7676ab5d644968435ebee131f2af2
victorav <victoravtr@gmail.com>
2020-12-05 15:11:48 +0100

    Update join_domain.ps1

--- Web-Proyecto-Content/assets/scripts/join_domain.ps1
+++ Web-Proyecto-Content/assets/scripts/join_domain.ps1
@@ -5,13 +5,17 @@ param (
     [string]$dom_pass = $arg[4],
     [string]$dom_name = $arg[5]
 )
-# Primero tenemos que conseguir el InterfaceIndex de la interfaz de red de la IP que ha introducido el usuario
+# Recogemos la password del archivo pass.txt y lo borramos.
 $password_file = Get-Content -Path .\pass.txt
 Remove-Item -Path .\pass.txt
+
+# Buscamos el Index de la interfaz de red y cambiamos sus DNS por la IP del controlador de dominio.
 $InterfaceIndex = Get-NetIPAddress -IPAddress $cli_ip | findstr InterfaceIndex
 $InterfaceIndex = $InterfaceIndex.Split(":").replace(' ','')
 $InterfaceIndex = $InterfaceIndex[1]
 Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses ($dom_ip)
+
+# Metemos el equipo en el dominio
 $password = $( ConvertTo-SecureString  -String $password_file -AsPlainText -Force )
 $credential = New-Object System.Management.Automation.PSCredential -ArgumentList $dom_user, $password
 Add-Computer -DomainName $dom_name -Credential $credential -Restart
